f89a56ad2aaf229f2dcf785617ba2b151d877b25
qifeng1994 <maidoufengfeng@gmail.com>
2022-04-30 23:17:07 +0900

    update no.34 wrong answer ;(

--- 34.cpp
+++ 34.cpp
@@ -1,5 +1,5 @@
 //nums=[], target=0
-class Solution_1 {
+class Solution_0 {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
 
@@ -31,3 +31,31 @@ public:
         return {left,right};
     }
 }
+
+class Solution_1 {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int left = LeftBound(nums,target);
+        int right = LeftBound(nums,target+1)-1;
+
+        //why must be if(left<nums.size() && nums[left]==target) ???
+        if(nums[left]==target && left<nums.size() )       
+            return {left,right};
+        else
+            return {-1,-1};
+    }
+
+    int LeftBound(vector<int>& nums, int target)
+    {
+        int left = 0, right = nums.size()-1;
+        while(left<=right)
+        {
+            int middle = left+(right-left)/2;
+            if(nums[middle] < target)           
+                left = middle+1;
+            else
+                right = middle-1;
+        }
+        return left;
+    }
+};
